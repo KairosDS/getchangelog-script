529b74f8ce0ef4dca57a9f1769933bd04c0f73d7
David de la Calle Cerezo <david.delacalle@kairosds.com>
2020-05-22 14:51:19 +0200

    Posibilidad de usar repos locales

--- get-evidencias.sh
+++ get-evidencias.sh
@@ -12,7 +12,8 @@
 set -euo pipefail
 
 AUTHOR=$1
-DELETEREPO=${2:-"false"}
+PULL=${2:-"true"}
+DELETEREPO=${3:-"false"}
 
 REPOSITORIES=$(cat repositories)
 WORKSPACE="$(pwd)"
@@ -30,6 +31,10 @@ println() {
     echo -e "[$dt] $1"
 }
 
+strindex() {
+  x="${1%%$2*}"
+  [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
 
 draw_progress_bar() {
   # Arguments: current value, max value
@@ -54,49 +59,71 @@ draw_progress_bar() {
 createEvidences() {
     echo
     git config --global credential.helper 'cache --timeout=9000'
-    cd $GITLAB_DIRECTORY
     for urlrepo in $REPOSITORIES; do
-        repository=$(echo ${urlrepo##*/})
-        if [ ! -d "$repository" ]; then
-            mkdir -p $repository
-            cd $repository
+
+        repositoryName=$(echo ${urlrepo##*/})
+
+        ## Local or url?
+        firstDot=$(strindex $urlrepo ".")
+        firstSlash=$(strindex $urlrepo "/")
+        if [ "$firstDot" = "0" ] || [ "$firstSlash" = "0" ]; then
+            echo "Local repo $repositoryName"
+            cd $WORKSPACE
+            repositoryDir=$urlrepo
+        else
+            echo "Remote repo $repositoryName"
+            cd $GITLAB_DIRECTORY
+            repositoryDir=$repositoryName
+        fi
+
+        # Clone or pull
+        if [ ! -d "$repositoryDir" ]; then
             println "Cloning repository $urlrepo "
+            mkdir -p $repositoryDir
+            cd $repositoryDir
             git clone -q $urlrepo.git .
         else
-            cd $repository
+            println "Change directory $repositoryDir"
+            cd $repositoryDir
             branch=$(git branch -a | grep -v "remotes" | grep -v detached | head -1 | tr -d "*" | awk '{print $1}')
-            println "Pulling repository $repository in branch $branch"
+            println "Pulling repository $repositoryName in branch $branch"
             git checkout -f -q $branch
-            git pull -q
+            if [ "$PULL" = "true"  ]; then
+                git pull -q
+            fi
         fi
+
         Totalbranches=$(git branch -a | wc -l)
-        println "Numbers of Branches in $urlrepo : $Totalbranches."
-        println "Searching for commits"
+        println "Numbers of Branches in $repositoryName: $Totalbranches."
+        println "Searching for commits..."
+        repositoryCommitLog="$GITLOGS_DIRECTORY/${repositoryName}_commits.log"
+
         i=0
         for branch in $(git branch -a | sed 's|\*||g' | awk '{print $1}')
         do
             git checkout -f -q $branch
-	        git log --pretty=format:"%h%x09%cd%x09%cn%x09%s" -i --committer="${AUTHOR}" --no-merges | awk -v PROJECT=$repository '{print $3" "$4 "\t" PROJECT "\t" $0}' | tail -r >> "$GITLOGS_DIRECTORY/${repository}_commits.log"
+            git log --pretty=format:"%h%x09%cd%x09%cn%x09%s" -i --committer="${AUTHOR}" --no-merges | awk -v PROJECT=$repositoryName '{print $3" "$4 "\t" PROJECT "\t" $0}' | tail -r >> "${repositoryCommitLog}"
             draw_progress_bar $i $Totalbranches
             i=$(( i + 1 ))
         done
+
         draw_progress_bar $Totalbranches
         echo
-        cat "$GITLOGS_DIRECTORY/${repository}_commits.log" | sort -u -o "$GITLOGS_DIRECTORY/${repository}_commits.log"
-        TotalCommits=$(cat $GITLOGS_DIRECTORY/${repository}_commits.log | wc -l)
-        println "Repository $repository Total commits: $TotalCommits"
+        cat "${repositoryCommitLog}" | sort -u -o "${repositoryCommitLog}"
+        TotalCommits=$(cat ${repositoryCommitLog} | wc -l)
+        println "Repository $repositoryName Total commits: $TotalCommits"
         cd $GITLAB_DIRECTORY
         if [ "$TotalCommits" = "0"  ] && [ "$DELETEREPO" = "true"  ];then
-		println "Removing $urlrepo from list"
-                echo $urlrepo >> $WORKSPACE/EmptyRepositories
-                cat $WORKSPACE/EmptyRepositories | sort -u -o $WORKSPACE/EmptyRepositories
-                sed -i "\|^${urlrepo}$|d" $WORKSPACE/repositories
-                rm -rf $GITLAB_DIRECTORY/$repository
+            println "Removing $urlrepo from list"
+            echo $urlrepo >> $WORKSPACE/EmptyRepositories
+            cat $WORKSPACE/EmptyRepositories | sort -u -o $WORKSPACE/EmptyRepositories
+            sed -i "\|^${urlrepo}$|d" $WORKSPACE/repositories
+            rm -rf $GITLAB_DIRECTORY/$repositoryName
         else
-		for MONTH in Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dic
-		do
-	            grep "^${MONTH}" "$GITLOGS_DIRECTORY/${repository}_commits.log" > "${WORKSPACE}/gitlogs/${MONTH}_${repository}_commits.log" && echo -n "${MONTH} " || rm -f "${WORKSPACE}/gitlogs/${MONTH}_${repository}_commits.log"
-		done
+        for MONTH in Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dic
+        do
+            grep "^${MONTH}" "${repositoryCommitLog}" > "${WORKSPACE}/gitlogs/${MONTH}_${repositoryName}_commits.log" && echo -n "${MONTH} " || rm -f "${WORKSPACE}/gitlogs/${MONTH}_${repositoryName}_commits.log"
+        done
         fi
         echo
     done
